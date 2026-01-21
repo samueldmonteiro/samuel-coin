a33d6dad5d969f022a4928240d4e5d5fd1c16ce8
Samuel D. Monteiro <samuel.dvmonteiro@gmail.com>
2026-01-21 17:26:15 -0300

    chore: adicionar prisma orm ao projeto

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './infra/db/prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaService) { }
+
+  async getHello(): Promise<any> {
+    return await this.prisma.wallet.findMany({});
   }
 }
